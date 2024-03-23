b7a0d85ed5bc1d408f222acefbf9e50b9cc340ae
mcdonaugh <brianmcdonaugh@gmail.com>
2024-03-23 15:33:14 -0400

    Added GameState Controller, Scoreview, and debug increment function

--- Assets/Scripts/Controllers/GameStateController.cs
+++ Assets/Scripts/Controllers/GameStateController.cs
@@ -1,8 +1,30 @@
+using SpaceInvaders.Views;
 using UnityEngine;
 
 namespace SpaceInvaders.Controllers
 {
     public class GameStateController : MonoBehaviour
     {
+        [SerializeField] private ScoreView _scoreView;
+        private int _playerScore;
+
+        private void Awake()
+        {
+            _playerScore = 0;
+        }
+        private void Update()
+        {
+            IncreaseScore();
+        }
+
+        private void IncreaseScore()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                _playerScore++;
+                Debug.Log($"{_playerScore}");
+            }
+        }
+        
     }
 }
---
+++ Assets/Scripts/Views/ScoreView.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace SpaceInvaders.Views
+{
+    public class ScoreView : MonoBehaviour
+    {
+
+    }
+}
