aa7b9881dc62cbb1bc96d114ee55af9a362f8424
mcdonaugh <brianmcdonaugh@gmail.com>
2024-03-25 22:18:11 -0400

    Refactored InvaderController into CreateGrid, SpawnInvader, StartMoveInterval, and Move

--- Assets/Scripts/Controllers/InvaderController.cs
+++ Assets/Scripts/Controllers/InvaderController.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class InvaderController : MonoBehaviour
+namespace SpaceInvaders.Controllers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InvaderController : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
