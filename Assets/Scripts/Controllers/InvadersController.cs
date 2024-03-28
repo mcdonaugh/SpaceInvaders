e995e9ca8546cb0f7f104e17830504d9868e4395
mcdonaugh <brianmcdonaugh@gmail.com>
2024-03-28 18:14:16 -0400

    added initial movement script for invaders

--- Assets/Scripts/Controllers/InvadersController.cs
+++ Assets/Scripts/Controllers/InvadersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace SpaceInvaders.Controllers
@@ -9,10 +11,14 @@ namespace SpaceInvaders.Controllers
         [SerializeField] private int _gridCols;
         [SerializeField] private float _gridSpacing;
         [SerializeField] private GameObject[] _invaderTypeArray;
+        [SerializeField] private int movementStep;
+        [SerializeField] private float moveDistanceSide;
+        [SerializeField] private float moveDistanceDown;
         private GameObject[] _invadersArray;
         private GameObject[] _invaderRowArray;
-        [SerializeField] private int _invadersRemaining;
+        private int _activeInvadersRemaining;
         private int invaderIndex;
+        
 
         private void Awake()
         {
@@ -27,53 +33,124 @@ namespace SpaceInvaders.Controllers
 
         private IEnumerator CreateGrid(int rows, int columns)
         {
-            if (invaderIndex < rows)
+
+            for (int row = 0; row < rows; row++)
             {
-                for (int row = 0; row < rows; row++)
+                GameObject newRow = new GameObject ($"Invader Row {row}");
+                _invaderRowArray[row] = newRow;
+            
+                for (int col = 0; col < columns; col++)
                 {
-                    GameObject newRow = new GameObject ($"Invader Row {row}");
-                    _invaderRowArray[row] = newRow;
-                
-                    for (int col = 0; col < columns; col++)
-                    {
-                        yield return new WaitForSeconds(.025f);
-                        Vector3 spawnPosition = new Vector3(transform.position.x + _gridSpacing * col, transform.position.y + _gridSpacing * row, transform.position.z);
-                        GameObject newInvader = SpawnInvader(spawnPosition, invaderIndex, newRow.transform);
-                        _invadersArray[col] = newInvader;
-                    }
-                    
-                    invaderIndex++;
+                    Vector3 spawnPosition = new Vector3(transform.position.x + _gridSpacing * col, transform.position.y + _gridSpacing * row, transform.position.z);
+                    GameObject newInvader = SpawnInvader(spawnPosition, invaderIndex, newRow.transform);
+                    _invadersArray[col] = newInvader;
+                    yield return new WaitForSeconds(.025f);
+                    ActivateInvader(newInvader);
                 }
+                
+                invaderIndex++;
             }
+
             StartCoroutine(StartMoveInterval());
+
         }
 
         private GameObject SpawnInvader(Vector3 position, int typeIndex, Transform parent)
         {   
             GameObject newInvader = Instantiate(_invaderTypeArray[typeIndex], position, Quaternion.identity);
             newInvader.transform.SetParent(parent);
-            _invadersRemaining++;
+            newInvader.SetActive(false);
             return newInvader;
+        }
+
+        private void ActivateInvader(GameObject invaderToActivate)
+        {
+            invaderToActivate.SetActive(true);
+            _activeInvadersRemaining++;
         }
 
         private IEnumerator StartMoveInterval()
         {
-            float moveInterval = 60/_invadersRemaining;
+            movementStep = 8;
+            bool moveRight = true;
+            float moveInterval = 60/_activeInvadersRemaining;
 
             while (true)
             {
-                yield return new WaitForSeconds(moveInterval);
 
-                foreach (var row in _invaderRowArray)
+                if (moveRight)
                 {
-                    Move(row);
-                } 
+                    yield return new WaitForSeconds(moveInterval);
+
+                    foreach (var row in _invaderRowArray)
+                    {
+                        yield return new WaitForSeconds(.1f);
+                        row.transform.position += new Vector3 (moveDistanceSide,0f,0f);
+                    } 
+
+                    movementStep++;
+                    Debug.Log(movementStep);
+                }
+
+                if (!moveRight)
+                {
+                    yield return new WaitForSeconds(moveInterval);
+
+                    foreach (var row in _invaderRowArray)
+                    {
+                        yield return new WaitForSeconds(.1f);
+                        row.transform.position -= new Vector3 (moveDistanceSide,0f,0f);
+                        
+                    } 
+
+                    movementStep--;
+                    Debug.Log(movementStep);
+                }
+
+                if (movementStep == 16)
+                {   
+                    yield return new WaitForSeconds(moveInterval);
+                    
+                    foreach (var row in _invaderRowArray)
+                    {
+                        yield return new WaitForSeconds(.1f);
+                        row.transform.position += new Vector3 (0f,-moveDistanceDown,0f);
+                    }
+
+                    yield return new WaitForSeconds(moveInterval);
+
+                    foreach (var row in _invaderRowArray)
+                    {
+                        yield return new WaitForSeconds(.1f);
+                        row.transform.position -= new Vector3 (moveDistanceSide,0f,0f);   
+                    }
+
+                    moveRight = false; 
+                }
+
+                if (movementStep == 0)
+                {   
+                    yield return new WaitForSeconds(moveInterval);
+                    
+                    foreach (var row in _invaderRowArray)
+                    {
+                        yield return new WaitForSeconds(.1f);
+                        row.transform.position += new Vector3 (0f,-moveDistanceDown,0f);
+                    }
+
+                    yield return new WaitForSeconds(moveInterval);
+
+                    foreach (var row in _invaderRowArray)
+                    {
+                        yield return new WaitForSeconds(.1f);
+                        row.transform.position += new Vector3 (moveDistanceSide,0f,0f);   
+                    }
+
+                    moveRight = true; 
+                }
+                
             }  
         }
 
-        private void Move(GameObject objectToMove)
-        {
-            objectToMove.transform.position += new Vector3 (.1f,0f,0f);
-        }
     }
 }
