05d55d3e13a97758b9ca8257a56312f59a0c26b8
mcdonaugh <brianmcdonaugh@gmail.com>
2024-04-01 15:56:20 -0400

    Added in re-factored grid generation and movement system

--- Assets/Scripts/Controllers/GameStateController.cs
+++ Assets/Scripts/Controllers/GameStateController.cs
@@ -18,6 +18,19 @@ namespace SpaceInvaders.Controllers
         [SerializeField] private TMP_Text _playerScoreText;
         private int _highScore;
         [SerializeField] private TMP_Text _highScoreText;
+        [SerializeField] private GameObject _enemyController;
+        [SerializeField] private int _gridRows;
+        [SerializeField] private int _gridCols;
+        [SerializeField] private float _gridOffset;
+        [SerializeField] private float _enemyBoundOffset = 3;
+        [SerializeField] private float _moveSpeed = .3f;
+        [SerializeField] private float _moveInterval = .2f;
+        private GameObject _topCenterEnemy;
+        private float _directionX = 1;
+        private float _directionY = 1;
+        private float _rowsMoved = 1;
+        private int _currentRowIndex;
+        private GameObject[,] _gridArray;
 
 
         private bool _gameIsActive;
@@ -78,6 +91,76 @@ namespace SpaceInvaders.Controllers
             _gameEndView.gameObject.SetActive(false);
             UpdateLivesText();
             UpdateScoreText();
+
+             _gridArray = new GameObject[_gridRows, _gridCols];
+            float rowWidth = (_gridCols - 1) * _gridOffset;
+            rowWidth /= 2;
+
+            float rowHeight = (_gridRows - 1) * _gridOffset;
+            rowHeight /= 2;
+
+            for (int i = 0; i < _gridRows; i++)
+            {
+                for (int j = 0; j < _gridCols; j++)
+                {
+                    GameObject newEnemy = Instantiate(_enemyController, new Vector3(-rowWidth, -rowHeight,0) + new Vector3(j * _gridOffset, i * _gridOffset, 0), Quaternion.identity);
+                    _gridArray[i, j] = newEnemy;
+                }
+            }
+
+            _topCenterEnemy = _gridArray[_gridRows - 1, _gridCols/2];
+            StartCoroutine(MoveTimer(_moveInterval));
+        }
+
+        private void MoveX()
+        {
+            
+            for (int i = 0; i < _gridCols; i++)
+            {
+                _gridArray[_currentRowIndex,i].transform.position += new Vector3(_directionX, 0 ,0) * _moveSpeed;
+            }
+
+            _currentRowIndex++;
+
+            if (_currentRowIndex == _gridRows)
+            {
+                _currentRowIndex = 0;
+            }
+
+            if (_topCenterEnemy.transform.position.x > _enemyBoundOffset)
+            {
+                MoveY();
+                _directionX = -1;
+            }    
+            else if (_topCenterEnemy.transform.position.x < -_enemyBoundOffset)
+            {
+                MoveY();
+                _directionX = 1;  
+            }        
+        }
+
+        private void MoveY()
+        {
+            bool moveDown = true;
+
+            if(moveDown == true)
+            {
+                for (int i = 0; i < _gridCols; i++)
+                {
+                    _gridArray[_currentRowIndex,i].transform.position += new Vector3(0, -_directionY ,0) * _moveSpeed;
+                }
+                moveDown = false;
+                _rowsMoved++;
+            }
+        }
+
+        private IEnumerator MoveTimer(float interval)
+        {
+            while(true)
+            {
+                yield return new WaitForSeconds(interval);
+                MoveX();
+            }
         }
 
         private void EndGame()
